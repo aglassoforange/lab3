662e1baf71fc1efec32e346bd63de9ceb9929050
aglassoforange <46038773+aglassoforange@users.noreply.github.com>
2023-04-20 18:47:35 -0700

    Yao's change

--- ArrayTests.java
+++ ArrayTests.java
@@ -12,7 +12,8 @@ public class ArrayTests {
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+
   }
 }
